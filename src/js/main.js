7be9b983c7fb123f459c4db498c24815e55a3dc7
01eksandr0 <krux228@gmail.com>
2024-05-12 23:17:49 +0300

    1

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 import './utils/vh.utils.js';
-import './utils/animate-on-view.utils'
+import './utils/animate-on-view.utils';
 
 import './components/uploadFile.component.js';
 import './components/inputs.component.js';
 
 import './libs/countUp.lib.js';
 
+import './utils/hero.js';
